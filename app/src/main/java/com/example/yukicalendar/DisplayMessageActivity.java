01cc2f87756daa38b0ca318c00457eeaf7353853
Esther <angaihvanchhawng@gmail.com>
2017-02-15 01:04:54 +0530

    Resolved merge conflict by incorporating both suggestions.

--- app/src/main/java/com/example/yukicalendar/DisplayMessageActivity.java
+++ app/src/main/java/com/example/yukicalendar/DisplayMessageActivity.java
@@ -3,7 +3,11 @@ package com.example.yukicalendar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -12,5 +16,29 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        EditText myEditText = (EditText) findViewById(R.id.edit_message);
+        myEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    Log.d("myText", s);
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        }
+
     }
 }
